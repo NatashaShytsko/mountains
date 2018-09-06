7d2c85d7e407eb3eee74783aee772e4bb214374f
Natasha Shytsko <natashashitko@gmail.com>
2018-09-06 12:04:30 +0300

    Changed button

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function setEqualHeight(columns) {
+    var tallestcolumn = 0;
+    columns.each(
+        function () {
+            currentHeight = $(this).height();
+            if (currentHeight > tallestcolumn) {
+                tallestcolumn = currentHeight;
+            }
+        }
+    );
+    columns.height(tallestcolumn);
+}
+
+$(document).ready(function () {
+    setEqualHeight($(".revcards>.col-md-4>.card"));
+});
+
+$(document).ready(function () {
+    $('span.navbar-toggle').click(function (e) {
+        $(this).toggleClass('active');
+        $('.head').toggle();
+        e.stopPropagation();
+    });
+    $('body').click(function () {
+        var link = $('span.navbar-toggle');
+        if (link.hasClass('active')) {
+            link.click();
+        }
+    });
+});
